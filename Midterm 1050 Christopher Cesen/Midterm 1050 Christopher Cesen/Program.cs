955b55869378429ff3f7948ff53fe1c05bcd16a8
IT1050ChristopherCesen <43357952+IT1050ChristopherCesen@users.noreply.github.com>
2018-11-14 17:45:28 -0500

    Add files via upload

---
+++ Midterm 1050 Christopher Cesen/Midterm 1050 Christopher Cesen/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm_1050_Christopher_Cesen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Question 1
+   {
+            bool keepLooping = true;
+            while (keepLooping == true)
+            {
+                Console.WriteLine("The Condition is"  + keepLooping);
+            }
+
+        }
+    }
+}
+
+Question 2
+	     {
+                int i = 2;
+                while (i < 16)
+
+                {
+
+                    Console.WriteLine(i);
+                    int control = 2;
+
+                    while (i < 128)
+                    {
+                        Console.WriteLine("[" + ((i * control) * 2) + "]");
+                        i++;
+                        /*}
+                        int product = i * control;
+                        int j = product * control;
+                        while (i <128)
+                        {
+                            Console.WriteLine("["+j+"]");
+                        }
+                        
+        }
+
+
+    }
+}
+Question 3
+     {
+            for (int i = 49; i > 0; i = i - 1)
+           /* {
+                /* if (i == 1)
+                 {
+                     continue;
+                 }
+             }
+             
+            }
+            {
+                Console.Write(i + ",");
+
+            }
+       {
+                Console.WriteLine();
+            
+            }
+	 }
+
+    }
+}
+
+
+Question 4			
+      {
+            int i = 1;
+            while(i<22)
+            {
+                if (i % 2 == 1)
+                    Console.Write(i+”\t”);
+                   ;
+                i++;
+                //String.Format("{0}\t{1}", i);
+            }
+        }
+    }
+}
+
+Question 5
+		Do-while
+            int n = 8;
+int i = 10;
+            do 
+            {
+                Console.Write("*");
+                i++;
+            } while (i<n);
+        }
+    }
+}
+
+While Loop
+ int n = 8;
+int i = 10;
+            while (n<i)
+            {
+                Console.WriteLine("*");
+                i++;
+            }
+        }
+* The do while loops produces a single asterik, The while loop writes asterixes constalty because the while codition is always true.
+
+
+Question 6
+bool icyRain = false;
+bool tornadoWarning = false; 
+            if (icyRain ==true || (tornadoWarning ==true))
+            {
+                Console.WriteLine("Let's stay inside.");
+            }
+                    if (icyRain == false && (tornadoWarning == true))
+            {
+                Console.WriteLine("Let's stay inside.");
+            }
+
+            if (icyRain == true && (tornadoWarning == false))
+            {
+                Console.WriteLine("Let's stay inside.");
+            }
+
+            if (icyRain == false && (tornadoWarning == false))
+            {
+                Console.WriteLine("Let's go outside");
+            }
+        }
+        }
+    }
+
+
+        }
+    }
+}
+*/
